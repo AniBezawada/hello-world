e19d3c71040d816739a0d650e72ec002b18970b8
AniBezawada <passion2fassion@gmail.com>
2017-02-17 14:02:42 +0530

    Add files via upload

---
+++ FirstDemo.java
@@ -0,0 +1,23 @@
+
+public class FirstDemo{
+	int a = 50;
+	int b = 25;
+	int c;
+	public static void main(String args[])
+		{
+			c = a+b;
+			System.out.println("Heyyy yayyy it's my first program");
+			System.out.println("addition of a+b" +c);
+			System.out.println("Heyyy yayyy it's my first program");
+			System.out.println("Heyyy yayyy it's my first program");
+			System.out.println("Heyyy yayyy it's my first program");
+			/* Naming Conventions
+				Classname
+				methodName
+				InterfaceName
+				variableName
+				packagename
+				CONSTANTS
+			*/
+		}
+}
